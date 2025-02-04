7d811efdb1c1ea87045a01480cae41bfb34b65d3
LeonardoTosinPR <leonardotosincomercial@gmail.com>
2025-02-04 19:27:05 -0300

    primeiro html e framework

---
+++ jogo.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.dropdown-trigger');
+    const instances = M.Dropdown.init(elems, {
+      // specify options here
+    });
+  });
